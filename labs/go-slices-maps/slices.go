3f41dfa4955c9ad96f1a4bdfca864bbe504ba0ce
Juantorresma <31540688+Juantorresma@users.noreply.github.com>
2019-09-10 20:37:57 -0500

    Update slices.go

--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,28 @@
 package main
 
+import "code.google.com/p/go-tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+
+	slice := make([][]uint8, dy)
+	
+	for x := 0; x < dy; x++ {
+	
+		slice[x] = make([]uint8, dx)
+		
+	}
+  
+	for x := 0; x < dy; x++ {
+	
+		for y := 0; y < dx; y++ {
+		
+			slice[x][y] = uint8(x*y)
+			
+		}
+	}
+	return slice
+}
+
 func main() {
+	pic.Show(Pic)
 }
