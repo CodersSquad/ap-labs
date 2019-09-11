3ad7c7611e9a47837b81c8ab46f4703d41e31392
Juantorresma <31540688+Juantorresma@users.noreply.github.com>
2019-09-10 20:43:51 -0500

    Update maps.go

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,31 @@
 package main
 
+import (
+
+	"golang.org/x/tour/wc"
+	
+	"strings"
+	
+)
+
+func WordCount(s string) map[string]int {
+
+	maps:=make(map[string]int)
+	
+	string := strings.SplitAfter(s," ")
+	
+	for i:=0;i<len(string);i++{
+	
+		maps[strings.Trim(string[i]," ")]+=1
+		
+	}
+	
+	return maps
+	
+}
+
 func main() {
+
+	wc.Test(WordCount)
+	
 }
