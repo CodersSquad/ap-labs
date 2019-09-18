2d39624d440382e7853b6f4aeaffdcc962fdac4e
Juantorresma <31540688+Juantorresma@users.noreply.github.com>
2019-09-17 22:08:55 -0500

    Update main.c

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include "strlib.c"
 
-int main() {
+
+int main(int argc, char **argv){
+    switch(argc){
+        case 2:
+        printf("Initial Length      : %d\n", mystrlen(argv[1]));
+        break;
+        case 3:
+        printf("Initial Length      : %d\n", mystrlen(argv[1]));
+        printf("New String          : %s\n", mystradd(argv[1], argv[2]));
+        break;
+        case 4:
+        printf("Initial Length      : %d\n", mystrlen(argv[1]));
+        printf("New String          : %s\n", mystradd(argv[1], argv[2]));
+        if(mystrfind(argv[1], argv[3]) == 1){
+            printf("SubString was found : yes\n");
+        } else {
+            printf("SubString was found : no\n");
+        }
+        break;
+        
+            }
     return 0;
 }
