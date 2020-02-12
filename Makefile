ac92e2b4cadbfe64520d65b573fc9b16ab9a01bf
Obed Muñoz <obed.n.munoz@gmail.com>
2020-02-12 16:12:23 -0600

    Add support for token and class id
    Signed-off-by: Obed Muñoz <obed.n.munoz@gmail.com>

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@ NAME		  ?= Demo User
 GITHUB_USER       ?= demo
 SCHOOL_ID         ?= A00123456
 CLASSIFY_ENDPOINT ?= http://classify.obedmr.com/
+CLASS_ID           = 07184303-556d-46ea-ab9d-bd56a9305610
 EXECUTABLES        = curl jq
 
 all: user
@@ -12,9 +13,20 @@ deps:
          $(if $(shell which $(exec)),,$(error "There's no '$(exec)' binary in your PATH")))
 
 user: deps
-	curl -k -s -X POST -d "githubID=${GITHUB_USER}&name=${NAME}&schoolID=${SCHOOL_ID}" $(CLASSIFY_ENDPOINT)/users | jq
+	curl -k -s -X POST -d "githubID=${GITHUB_USER}&name=${NAME}&schoolID=${SCHOOL_ID}&class=${CLASS_ID}" $(CLASSIFY_ENDPOINT)/users | jq
 
 test: deps
 	@echo User Information
 	curl -k -s $(CLASSIFY_ENDPOINT)/users\?githubID\=$(GITHUB_USER)  | jq
 
+submit:
+	@echo Cannot submit your lab. You must be located in the lab directory
+	@echo Example:
+	@echo cd labs/first-challenge
+	@echo GITHUB_USER=demo make submit
+
+check-submission:
+	@echo Cannot check your submission. You must be located in the lab directory
+	@echo Example:
+	@echo cd labs/first-challenge
+	@echo GITHUB_USER=demo make check-submission
--- common.mk
+++ common.mk
@@ -1,13 +1,14 @@
 # Classify Requirements
+SHELL                = bash
 GITHUB_USER         ?= demo
 CLASSIFY_ENDPOINT   ?= http://classify.obedmr.com
 CLASSIFY_TOKEN      ?= demo
 CLASSIFY_TOKEN_FILE ?= ${HOME}/.classify_token
+CLASS_ID             = 07184303-556d-46ea-ab9d-bd56a9305610
 EXECUTABLES          = curl jq
 
 # Common Submission targets
-
-submit: deps
+submit: deps check-branch
 	@if [[ $$(git ls-files --others --modified --exclude-standard | wc -l) -gt 0 ]]; then \
 		echo "You have uncommitted files. Verify them and then, add and commit them."; \
 		make clean; \
@@ -18,13 +19,24 @@ submit: deps
 	@$(eval BRANCH := $(shell git rev-parse --abbrev-ref HEAD))
 	@$(eval COMMIT_ID := $(shell git log --format='%H' -n 1))
 	@$(eval CLASSIFY_TOKEN := $(shell [ -f ${CLASSIFY_TOKEN_FILE} ] && cat ${CLASSIFY_TOKEN_FILE} || echo ${CLASSIFY_TOKEN}))
+	@$(eval LAB_ID := $(shell curl -s -k ${CLASSIFY_ENDPOINT}/labs\?ClassID\=${CLASS_ID}\&RepoDir=$$(basename "$$PWD") | jq -r '.labs[0].ID'))
 
-	@curl -k -s -X POST -d "branch=$(BRANCH)&commit=$(COMMIT_ID)&token=$(CLASSIFY_TOKEN)" $(CLASSIFY_ENDPOINT)/labs/$(USER_ID)/$$(basename "$$PWD") | jq
+	@curl -k -s -X POST -d "branch=$(BRANCH)&commit=$(COMMIT_ID)&token=$(CLASSIFY_TOKEN)&class=$(CLASS_ID)" $(CLASSIFY_ENDPOINT)/labs/$(USER_ID)/${LAB_ID} | jq
 
 check-submission:
 	@$(eval USER_ID := $(shell curl -s -k ${CLASSIFY_ENDPOINT}/users\?githubID\=${GITHUB_USER} | jq -r '.users[0].ID'))
-	@curl -k -s  $(CLASSIFY_ENDPOINT)/labs/$(USER_ID)/$$(basename "$$PWD") | jq
+	@$(eval LAB_ID := $(shell curl -s -k ${CLASSIFY_ENDPOINT}/labs\?ClassID\=${CLASS_ID}\&RepoDir=$$(basename "$$PWD") | jq -r '.labs[0].ID'))
+	@curl -k -s  $(CLASSIFY_ENDPOINT)/labs/$(USER_ID)/${LAB_ID} | jq
 
 deps:
         $(foreach exec,$(EXECUTABLES),\
          $(if $(shell which $(exec)),,$(error "There's no '$(exec)' binary in your PATH")))
+
+check-branch:
+	@if [[ $$(git rev-parse --symbolic-full-name --abbrev-ref HEAD) = "master" ]]; then \
+		echo "You cannot make submissions on [master] branch," \
+		"please follow Classify API instructions to create a new branch for your lab and then submit your work."; \
+		echo "You can check the current branch you are on with:";  \
+		echo "git branch"; \
+		exit -1; \
+	fi
