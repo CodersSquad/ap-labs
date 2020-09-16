beb1aa41215a69eb5aae236666558f7808dd6880
Obed N Munoz <2779484+obedmr@users.noreply.github.com>
2020-09-15 20:45:43 -0500

    Update lab.mk

--- labs/go-functions-methods/lab.mk
+++ labs/go-functions-methods/lab.mk
@@ -4,13 +4,13 @@ build:
 	go build geometry.go
 
 test: build
-	@echo Test 1 - 2 sides - failed test
-	./geometry
-	@echo Test 2 - 4 sides
+	@echo Test 1 - 2 points - failed test
+	./geometry 2
+	@echo Test 2 - 4 points
 	./geometry 4
-	@echo Test 3 - 8 sides
+	@echo Test 3 - 8 points
 	./geometry 8
-	@echo Test 3 - 12 sides
+	@echo Test 3 - 12 points
 	./geometry 12
 
 clean:
