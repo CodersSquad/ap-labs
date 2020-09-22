d201a1e6667ee4190643efe2fc76de212b43d3cb
Obed Muñoz <obed.n.munoz@gmail.com>
2020-09-22 17:42:56 -0500

    Update test cases for advanced logger
    Signed-off-by: Obed Muñoz <obed.n.munoz@gmail.com>

--- labs/advanced-logger/lab.mk
+++ labs/advanced-logger/lab.mk
@@ -7,7 +7,15 @@ build:
 	gcc -c ${LIB_NAME}.c -o ${LIB_NAME}.o
 	gcc    ${LIB_NAME}.o ${APP_NAME}.o  -o ${APP_NAME}
 test: build
-	@echo Test 1
+	 @echo Test 1
+	./${APP_NAME} 1
+	@echo Test 2
+	./${APP_NAME} 2
+	@echo Test 3
+	./${APP_NAME} 3
+	@echo Test 4 - invalid test
+	./${APP_NAME} 4
+	@echo Test 4 - failed
 	./${APP_NAME}
 
 clean:
--- labs/advanced-logger/testLogger.c
+++ labs/advanced-logger/testLogger.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "logger.h"
 
-int main(){
+int main(int argc, char **argv){
 
-    // default logging
-    infof("INFO Message %d", 1);
-    warnf("WARN Message %d", 2);
-    errorf("ERROR Message %d", 2);
-
-    // stdout logging
-    initLogger("stdout");
-    infof("INFO Message %d", 1);
-    warnf("WARN Message %d", 2);
-    errorf("ERROR Message %d", 2);
-
-    // syslog logging
-    initLogger("syslog");
-    infof("INFO Message %d", 1);
-    warnf("WARN Message %d", 2);
-    errorf("ERROR Message %d", 2);
+    printf("%d\n", atoi(argv[1]));
+    switch(atoi(argv[1])) {
+    case 1:
+	// default logging
+	infof("INFO Message %d", 1);
+	warnf("WARN Message %d", 2);
+	errorf("ERROR Message %d", 2);
+	break;
+    case 2:
+	// stdout logging
+	initLogger("stdout");
+	infof("INFO Message %d", 1);
+	warnf("WARN Message %d", 2);
+	errorf("ERROR Message %d", 2);
+	break;
+    case 3:
+	// syslog logging
+	initLogger("syslog");
+	infof("INFO Message %d", 1);
+	warnf("WARN Message %d", 2);
+	errorf("ERROR Message %d", 2);
+	break;
+    default:
+	errorf("Invalid test case");
+	break;
+    }
 
     return 0;
 }
