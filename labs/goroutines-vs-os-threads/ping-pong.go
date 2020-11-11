de19fcf3077230f8e03721a6df3da0006a4a06eb
Andre Calderon <andre.calder98@gmail.com>
2020-11-10 21:06:35 -0600

    Lab Solution
    Signed-off-by: Andre Calderon <andre.calder98@gmail.com>

--- labs/goroutines-vs-os-threads/ping-pong.go
+++ labs/goroutines-vs-os-threads/ping-pong.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func main() {
-	var commsPerSecond int
+func connect(in chan int, out chan int) {
+	for {
+		out <- (1 + <-in)
+	}
+}
 
-	fmt.Println("Communications Per Second : ", commsPerSecond)
+func main() {
+	message1 := make(chan int)
+	message2 := make(chan int)
+	total := 0
+
+	go connect(message1, message2)
+	go connect(message2, message1)
+
+	for i := 0; i < 1; i++ {
+		message1 <- 0
+		time.Sleep(time.Duration(1) * time.Second)
+		x := <-message1
+		total += x
+	}
+
+	fmt.Println("Communications Per Second : ", total)
 }
--- labs/goroutines-vs-os-threads/pipeline.go
+++ labs/goroutines-vs-os-threads/pipeline.go
@@ -2,11 +2,43 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"time"
 )
 
-func main() {
-	var maxStages, transitTime int
+var memory runtime.MemStats
 
-	fmt.Println("Maximum number of pipeline stages   : ", maxStages)
-	fmt.Println("Time to transit trough the pipeline : ", transitTime)
+func pipeline(memoryLimit uint64) {
+
+	runtime.ReadMemStats(&memory)
+	channel := make(chan int)
+	current := 0
+	var memory runtime.MemStats
+
+	defer func() {
+		if goroutine := recover(); goroutine != nil {
+			fmt.Println("Maximum number of pipeline stages   : ", current)
+			startingTime := time.Now()
+			for ; current > 0; current-- {
+				channel <- current
+			}
+			fmt.Println("Time to transit trough the pipeline : ", time.Since(startingTime))
+		}
+
+	}()
+
+	for {
+		go func() {
+			current++
+			<-channel
+		}()
+		runtime.ReadMemStats(&memory)
+		if memory.TotalAlloc/1024/1024 > memoryLimit {
+			panic("Max Memory Reached")
+		}
+	}
+}
+
+func main() {
+	pipeline(1024)
 }
