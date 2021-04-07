ac713f32fe79989a4abc09be9e59029eb1d93a13
Obed Muñoz <obed.n.munoz@gmail.com>
2021-04-06 22:04:36 -0500

    Update test cases
    Signed-off-by: Obed Muñoz <obed.n.munoz@gmail.com>

--- labs/logger/lab.mk
+++ labs/logger/lab.mk
@@ -9,13 +9,7 @@ build:
 	gcc -c ${LIB_NAME}.c -o ${LIB_NAME}.o
 	gcc ${APP_NAME}.o ${LIB_NAME}.o -o ${EXE_NAME}.o
 test: build
-	@echo Test 1 - default
-	./${EXE_NAME}.o 1
-	@echo Test 2 - stdout
-	./${EXE_NAME}.o 2
-	@echo Test 3 - syslog
-	./${EXE_NAME}.o 3
-	@echo Test 4 - Invalid case
-	./${EXE_NAME}.o 4
+	@echo Test 1
+	./${EXE_NAME}.o
 clean:
 	rm -rf *.o
