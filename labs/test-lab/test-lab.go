48484306e5b27e69748b6eb632174a8bec6a1f58
Dalia <a01635883@itesm.mx>
2021-08-16 19:21:18 +0000

    done test

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+	if len(os.Args) > 1 {
+		var name strings.Builder
+		for i := 1; i < len(os.Args); i++ {
+			if i != len(os.Args)-1 {
+				name.WriteString(os.Args[i] + " ")
+			} else {
+				name.WriteString(os.Args[i])
+			}
+		}
+		fmt.Printf("Welcome %v to the jungle \n", name.String())
+	}else{
+		fmt.Println("Error, not enough arguments, try again")
+	}
 }
