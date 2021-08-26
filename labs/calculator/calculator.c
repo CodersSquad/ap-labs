6e7db687973bbbf6435f5709f84d5f0c72c85cf6
Obed N Munoz <obed.n.munoz@gmail.com>
2021-08-25 21:57:46 -0500

    Add calculator lab
    Signed-off-by: Obed N Munoz <obed.n.munoz@gmail.com>

---
+++ labs/calculator/calculator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// adds/subtracts/multiplies all values that are in the *values array.
+// nValues is the number of values you're reading from the array
+// operator will indicate if it's an addition (1), subtraction (2) or
+// multiplication (3)
+long calc(int operator, int nValues, int *values) {
+    return 0;
+}
+
+int main() {
+    return -1;
+}
