9a1d2b2e0ec9a49498bef179f2a006561e6ca811
Andrés Díaz de León <andruix93@gmail.com>
2021-09-04 04:46:31 +0000

    c problem solved

--- challenges/first-partial/c-month-day/month_day.c
+++ challenges/first-partial/c-month-day/month_day.c
@@ -1,8 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /* month_day function's prototype*/
 void month_day(int year, int yearday, int *pmonth, int *pday);
 
-int main() {
-    return 0;
+static char daytab[2][12] = {
+    {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+    {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+static char *months[12] = {
+    "January", "February", "March",
+    "April", "May", "June",
+    "July", "August", "September",
+    "October", "November", "December"
+};
+
+int main(int argc, char **argv) {
+    int pday;
+    int pmonth;
+    if(argc > 2){
+        //check days are no more than 366
+       
+        month_day(atoi(argv[1]), atoi(argv[2]), &pmonth, &pday);
+        if(pmonth != -1){
+            printf("%s %d, %s\n", months[pmonth], pday, argv[1]);
+        }else{
+            printf("Not a valid date\n");
+            return 0;
+        } 
+        return 0;
+    }
+    printf("Not enough arguments\n");
+    return -1;
+}
+
+//year and yearday are from argv
+//*pmonth and *pday will be returned as result
+void month_day(int year, int yearday, int *pmonth, int *pday){
+    //Checks if leap year
+    int leap;
+    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+
+    if(leap == 0 && yearday <= 365 || leap == 1 && yearday <= 366){
+       //gets date of year
+        for(int i = 0; i < 12; i++){
+            if(yearday <= daytab[leap][i]){
+                *pmonth = i;
+                *pday = yearday;
+                break;
+            }else{
+                yearday -= daytab[leap][i];
+            }
+        }    
+    }else{
+        *pmonth = -1;
+    }
 }
