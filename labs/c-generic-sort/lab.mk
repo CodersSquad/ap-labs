f8771f186770b8495b35784a03b3be29deae9ff1
Obed N Munoz <obed.n.munoz@gmail.com>
2021-10-11 12:47:44 -0500

    Update lab.mk

--- labs/c-generic-sort/lab.mk
+++ labs/c-generic-sort/lab.mk
@@ -9,11 +9,11 @@ build:
 	gcc quicksort.o mergesort.o ${APP_NAME}.o -o ${APP_NAME}
 test: build
 	@echo Test 1 - sort numbers quick sort
-	./${APP_NAME} -n numbers.txt -quicksort || true
+	./${APP_NAME} -n numbers.txt -quicksort -o qs_sorted_numbers.txt || true
 	@echo Test 2 - sort strings merge sort
-	./${APP_NAME} strings.txt -mergesort || true
+	./${APP_NAME} strings.txt -mergesort -o ms_sorted_strings.txt || true
 	@echo Test 3 - sort strings - quick sort
-	./${APP_NAME} strings.txt -quicksort || true
+	./${APP_NAME} strings.txt -quicksort -o qs_sorted_strings.txt || true
 	@echo Test 4 - handle missing parameters
 	./${APP_NAME} || true
 	@echo Test 5 - handle incomplete parameters
