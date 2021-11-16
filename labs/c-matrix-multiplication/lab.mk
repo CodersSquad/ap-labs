f53197b0fa6d9bf8832ff9774833b06452decde0
Obed N Munoz <obed.n.munoz@gmail.com>
2021-11-16 12:49:21 -0600

    Update last labs
    Signed-off-by: Obed N Munoz <obed.n.munoz@gmail.com>

--- labs/c-matrix-multiplication/lab.mk
+++ labs/c-matrix-multiplication/lab.mk
@@ -17,15 +17,15 @@ build:
 
 test: build data_files
 	 @echo Test 1
-	./${APP_NAME} -n 100 -out result_test1.txt
+	./${APP_NAME} -n 100 -out result_test1.txt | true
 	@echo Test 2
-	./${APP_NAME} -n 200 -out result_test2.txt
+	./${APP_NAME} -n 200 -out result_test2.txt | true
 	@echo Test 3
-	./${APP_NAME} -n 400 -out result_test3.txt
+	./${APP_NAME} -n 400 -out result_test3.txt | true
 	@echo Test 4
-	./${APP_NAME} -n 800 -out result_test4.txt
+	./${APP_NAME} -n 800 -out result_test4.txt | true
 	@echo Test 5
-	./${APP_NAME} -out result_test4.txt -n 100
+	./${APP_NAME} -out result_test4.txt -n 100 | true
 
 clean:
 	rm -rf *.o ${APP_NAME} *.dat
